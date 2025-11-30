b235bc25fef41792f5d3875b0a660dde657c1ae3
hugoFletes30 <hugitofletes30@gmail.com>
2025-11-30 18:14:58 -0500

    Update page.tsx

--- frontend/my-app2/app/add/page.tsx
+++ frontend/my-app2/app/add/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { API_URL } from "@/lib/api";
+import { API_URL } from "../lib/api";
 
 export default function AddProduct() {
   const [form, setForm] = useState({
